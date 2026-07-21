d7b24a435cee2127cce5bb74beadfd3060013419
MindCC <mindcc@users.noreply.github.com>
2026-07-21 21:47:38 +0800

    添加全部关卡文件及备份

---
+++ js/levels/level5_boss.js
@@ -0,0 +1,13 @@
+/* 第 5 关：Boss感染 */
+const LEVEL_5 = {
+  name: 'Boss感染',
+  width: 80,
+  cellType: 1,
+  winCondition: WIN_KILL_ALL,
+  sky: ['#2a0a0a','#6a0a0a'],
+  map: [],
+  tutorials: [],
+  floatPlatforms: [],
+  knowledgeCards: [],
+  pipeSpawners: [],
+};
